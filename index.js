2b08fd44ecbbe6e66d3524861d0ac41955022497
Francys Garcia <fmgarciadeveloper@gmail.com>
2018-11-15 23:55:14 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const amqp = require('amqplib/callback_api');
+let amqpConn = 'por iniciar';
+const conectionURL = process.env.CLOUDAMQP_URL || 'amqp://pxojdlzo:AAQwHpLaTEGXbGNw0eaVS2KAldUnM_Fm@baboon.rmq.cloudamqp.com/pxojdlzo';
+//process.env.CLOUDAMQP_URL + "?heartbeat=60"
+amqp.connect(conectionURL, function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var q = 'hello';
+
+    ch.assertQueue(q, {durable: false});
+    // Note: on Node 6 Buffer.from(msg) should be used
+    ch.sendToQueue(q, new Buffer('Hello World!'));
+    console.log(" [x] Sent 'Hello World!'");
+    ch.close(function() {conn.close()})
+  });
+});
+
+amqp.connect(conectionURL, function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var q = 'hello';
+
+    ch.assertQueue(q, {durable: false});
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+    ch.consume(q, function(msg) {
+      console.log(" [x] Received %s", msg.content.toString());
+      amqpConn = msg.content.toString()
+    }, {noAck: true});
+  });
+});
+
+app.get("/", function(request, response) {
+  console.log(amqpConn);
+  response.send(amqpConn);
+});
+
+app.listen(port, function() {
+  console.log("Express app started on port "+ port);
+});
